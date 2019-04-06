89eed0f9ed2c6352d04bd87c0bebf5c725ab2842
dlobba <diego.lobba@studenti.unitn.it>
2019-04-06 13:21:14 +0200

    generate msc file for execution

--- parser.py
+++ parser.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 import re
 import sys
+import operator
 
 from functools import reduce
 
 import total_order2 as to
 import data_analysis as da
+
+from msc_helper import MSCHelper
 
 class ParseEventException(Exception):
     def __init__(self, message=None):
@@ -135,8 +138,9 @@ def wrong_exec_parser(log_file, msg_filter_func=lambda x: x is not None):
 
     Return
     ------
-    A list of items `<system_time, source, destination, label>` in order
+    An ordered list of items `<system_time, source, destination, label>`
     to produce a (hopefully) valid mscgen chart.
+    The list is sorted by system_time.
     """
     actor_regexp = re.compile("INFO:\s+EpTO:\s+(\w+)\s+at_(\d+)_(\d+)\s+(\w+)\s+(.*)")
     # collect information
@@ -152,14 +156,38 @@ def wrong_exec_parser(log_file, msg_filter_func=lambda x: x is not None):
                 rec_ball_reg = re.compile(r"\s*(\w+)\s+{\s+([^}]*)\s+}")
                 receiver, events = rec_ball_reg.match(argument).groups()
                 events_label = parse_ball(argument)
-                data.append((gclock, actor, receiver, "received_ball " + events_label))
+                data.append((gclock, actor, receiver, "received_ball\\n\\n " + events_label))
             elif "received_set" in action:
                 events_label = parse_ball(argument)
-                data.append((gclock, actor, actor, "received_set " + events_label))
+                data.append((gclock, actor, actor, "received_set\\n\\n " + events_label))
             if "deliverable_set" in action:
                 events_label = parse_ball(argument)
-                data.append((gclock, actor, actor, "deliverable_set " + events_label))
-    return data
+                data.append((gclock, actor, actor, "deliverable_set\\n\\n " + events_label))
+            if "delivered" in action:
+                ts, source, msg_id = parse_event(argument)
+                message = "{}:{}".format(source,msg_id)
+                data.append((gclock, actor, actor, "delivered\\n\\n " + message))
+                
+    return sorted(data, key=operator.itemgetter(0))
+
+def make_msc(csv_data):
+    h = MSCHelper()
+    for _, source, destination, label in csv_data:
+        new_lines = label.split(",")
+        label = str.join("\\n"*2, new_lines)
+        h.add_entity(source)
+        h.add_entity(destination)
+        if "delivered" in label:
+            color = "#ff0000"
+        else:
+            color = "#000000"
+        h.add_message(source, destination, label=label, linecolor=color)
+        h.new_line()
+        for x in range(0, 2):
+            h.add_gap()
+            h.new_line()
+        h.end_line()
+    return h
 
 if __name__ == "__main__":
     """
@@ -171,11 +199,14 @@ if __name__ == "__main__":
     4. the minimum and maximum delivery rate found, considering
        all the messages.
     """    
-    filter_func = lambda x: da.filter_msg(x, 10, 100)
+    #filter_func = lambda x: da.filter_msg(x, 10, 100)
+    filter_func = lambda x: False
     broadcast, msg_delivery, delivery_order, delta_msg = parse_logs(sys.argv[1], filter_func)
-    p2 = parse_logs2(sys.argv[1], filter_func)
+    p2 = wrong_exec_parser(sys.argv[1], filter_func)
+    k = make_msc(p2)
+    with open("msc.txt", "w") as fh:
+        fh.write(k.make_msc())
 
-    """
 
     actor_msg     = da.compute_actor_msg(msg_delivery)
     num_actors   = len(actor_msg.keys()) 
@@ -190,7 +221,7 @@ if __name__ == "__main__":
 
     print(summary(num_actors, epoch, num_messages, avg_msg,\
                   min_drate, max_drate))
-    
+    """
     delays1 = da.compute_delivery_delay(delta_msg)
     delays2 = list(da.delay_count(delays1, num_actors))
     delays3 = da.sum_delivery_fraction(delays2)
